a022837ec4983e89ac31306b42b7db857e7c2cf4
Nikolay Sheshin <nasheshin.portfolio@gmail.com>
2022-12-01 16:11:38 +0500

    Changed output of task3

--- task3.py
+++ task3.py
@@ -15,19 +15,37 @@ def main():
         return
 
     video_path = os.path.join('test_video', f'test_video_{video_index}.mkv')
+    markup_path = os.path.join('test_video', f'test_video_{video_index}.csv')
+    target_image_path = os.path.join('samples', f'sample{video_index}.png')
 
     cap = cv2.VideoCapture(video_path)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    data = read_markup(markup_path, (width, height))
  
-    if (cap.isOpened()== False):
+    if (cap.isOpened()== False): 
         print("Error opening video stream or file")
+    
+    target_image = cv2.imread(target_image_path)
+    tracker = Tracker("reidentification.onnx")
+    vehicle_identificator = VehicleIdentificator("reidentification.onnx")
+    target_emb = vehicle_identificator.get_embeddings([target_image])[0]
 
+    print("Gathering data...")
+    frame_index = 0
     frames = []
     while(cap.isOpened()):
         ret, frame = cap.read()
+        if frame_index in data:
+            tracker.update(data[frame_index], frame, frame_index)
         if ret == True:
-            frames.append(frame)
+            for trajectory in tracker.trajectories:
+                if vehicle_identificator.compare(trajectory.mean_emb(), target_emb) > 0.5:
+                    if trajectory.last_update_frame_index == frame_index:
+                        frames.append(cv2.resize(trajectory.nodes[-1].image, (416, 416)))
         else:
             break
+        frame_index += 1
     
     cleaner = DublicateCleaner()
     cleaned_frames = cleaner.clean(frames)
